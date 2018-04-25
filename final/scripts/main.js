7c3195e568ecde98181db5b78448718ba911f9f0
cron0039 <38300429+cron0039@users.noreply.github.com>
2018-04-25 17:18:19 -0400

    mostly done

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,3 +1,10 @@
 // this script file would have the JS that is shared between all pages
 // javascript for hamburger button
 // js for animation
+
+$('.hamburger_container').on('click', function(){
+
+		// .slideToggle(time) is the Jquery function that shows/hides elements with a slide animation 
+		// time is speficied to determine the duration of animation
+		$('.hidden_menu').slideToggle(300);
+	});
